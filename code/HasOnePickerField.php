71527f99d945031c85afc738e4e353bb792dc9fc
ajshort <andrewjshort@gmail.com>
2010-12-26 14:42:43 +1100

    ENHANCEMENT: Removed cache-avoidance code dependency in HasOnePickerField by manually clearing the cache.

--- code/HasOnePickerField.php
+++ code/HasOnePickerField.php
@@ -24,22 +24,13 @@ class HasOnePickerField extends HasManyPickerField {
 		$accessorfield = $this->name . 'ID';
 		$this->parent->$accessorfield = null;
 		$this->parent->write();
+		$this->parent->flushCache();
 
 		return Director::is_ajax() ? $this->FieldHolder() : Director::redirectBack();
 	}
 
 	public function Items() {
-		$accessor = $this->name;
-		$dos = new DataObjectSet();
-
-		// This is really nasty: there are two instances of the same db record. During HasOnePickerField::Remove()
-		// we release the has_one relation for one instance and then update the field from the other which still
-		// holds the hos_one set. This happens because only DataObject::get_one() calls get cached but not instances
-		// retrieved with DataObject::get();
-		$this->parent = DataObject::get($this->parent->ClassName, "\"ID\" = " . $this->parent->ID)->first();
-
-		if($this->parent->$accessor() && $this->parent->$accessor()->ID) $dos->push($this->parent->$accessor());
-		return $dos;
+		return $this->parent->{$this->name};
 	}
 
 	public function saveInto(DataObject $record) {
